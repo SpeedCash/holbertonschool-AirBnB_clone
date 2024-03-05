74d64c7d78b34f7e6f44ca6244b7bd358f200951
SpeedCash <tcravero@gmail.com>
2024-03-05 12:29:38 +0000

    the base model updated

--- models/base_model.py
+++ models/base_model.py
@@ -4,10 +4,16 @@ import uuid
 
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.fromisoformat(value)
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(
@@ -25,15 +31,28 @@ class BaseModel:
 
 
 if __name__ == "__main__":
+    from models.base_model import BaseModel
+
     my_model = BaseModel()
-    my_model.name = "My First Model"
+    my_model.name = "My_First_Model"
     my_model.my_number = 89
+    print(my_model.id)
     print(my_model)
-    my_model.save()
-    print(my_model)
+    print(type(my_model.created_at))
+    print("--")
     my_model_json = my_model.to_dict()
     print(my_model_json)
     print("JSON of my_model:")
     for key in my_model_json.keys():
         print("\t{}: ({}) - {}".format(
-            key, type(my_model_json[key]), my_model_json[key]))
+            key, type(my_model_json[key]), my_model_json[key]
+        ))
+
+    print("--")
+    my_new_model = BaseModel(**my_model_json)
+    print(my_new_model.id)
+    print(my_new_model)
+    print(type(my_new_model.created_at))
+
+    print("--")
+    print(my_model is my_new_model)
