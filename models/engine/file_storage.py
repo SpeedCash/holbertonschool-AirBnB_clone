86652428db5c71fb6194eca059e37a62976e011c
SpeedCash <tcravero@gmail.com>
2024-03-07 11:46:43 +0000

    the file storage updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,24 @@
+import cmd
+from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def __init__(self):
+        super().__init__()
+        self.classes = {
+            "BaseModel": BaseModel,
+            "User": User,
+            "Place": Place,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Review": Review
+        }
